fc7e1495ce5036f1786b40976a6f7f7b9fb5e0ed
Alessandro Stockman <alessandrostockman@gmail.com>
2021-04-20 09:57:06 +0200

    add json deserialization and standard form conversion

---
+++ lib/utility.py
@@ -0,0 +1,53 @@
+import numpy as np
+import json
+  
+def deserialize_problem(file_path):
+    with open(file_path) as json_file:
+        problem = json.load(json_file)
+    
+    constraints = []
+    constants = []
+    for index, constraint in enumerate(problem['constraints']):
+        constraints.append(constraint['coefficients'])
+        constants.append(constraint['constant'])
+
+    return problem, np.array(constraints), np.array(constants), np.array(problem['objective']['costs'])
+
+def get_standard_form(problem, A, b, c):
+    Ac = np.r_[[c], A]
+    rows, cols = Ac.shape
+
+    # 1. Change objective function to minimization
+    if problem['objective']['optmimization'] == 'MAX':
+        problem['objective']['costs'] *= -1
+        problem['objective']['optmimization'] = 'MIN'
+        Ac[0,:] *= -1
+
+    # 2. Perform variable change over non-positive variables
+    positive_variables = np.zeros(cols, dtype=bool)
+
+    for i in range(cols):
+        if i in problem['non-negatives']:
+            positive_variables[i] = True
+
+    for var in np.where(positive_variables == False):
+        Ac = np.c_[Ac, Ac[:, var + 1] * -1]
+
+    # 3. Add slack variables to change constraints into equations
+    for index, constraint in enumerate(problem['constraints']):
+        if constraint['type'] != 'EQ':
+            Ac = np.c_[Ac, np.zeros(rows)]
+            
+            if constraint['type'] == 'LEQ':
+                Ac[index + 1,-1] = 1
+            elif constraint['type'] == 'GEQ':
+                Ac[index + 1,-1] = -1
+
+    matrix = np.c_[Ac, np.insert(b, 0, 0)]
+
+    # 4. Constant terms should be positive or 0
+    for index, const in enumerate(b):
+        if const < 0:
+            matrix[index + 1, :] *= -1
+
+    return matrix
