a0aa3b4f5abd9a9a70a7e5cfef65c7f0b4018467
Riccardo De Matteo <51266633+riccardodm97@users.noreply.github.com>
2021-04-23 13:39:17 +0200

    concluded phase2

--- lib/simplex2.py
+++ lib/simplex2.py
@@ -58,12 +58,29 @@ def from_f1_to_f2(data_f1,data):
     #TODO 
     return 
 
+def determine_entering_var(data):
+    for j in data.out_base :
+        cj = data.c[j] + np.dot(data.y,data.A[:,j])
+        if cj < 0 : 
+            return cj,j
+    
+    return None,None 
+
+def determine_exiting_var(data,Aj):
+
+    arr = data.xb/Aj
+    h = np.where(arr > 0, arr, np.inf).argmin()
+
+    #TODO: qui tocca vedere che porco due ritorna np.where
+    out_index = h[data.in_base[h].argmin()]       #TODO: test 
+    
+    return out_index
+        
 def start_simplex(data):
 
     base_indexes = find_initial_basis(data.A)
     data.in_base = base_indexes 
-    compute_out_of_base(data)
-    data.set_inverse_matrix = np.identity(A.shape[0])
+    data.set_inverse_matrix = np.identity(data.A.shape[0])
 
     if -1 in base_indexes:
         phase1(data)
@@ -80,8 +97,39 @@ def phase1(data):
 
 def phase2(data):
 
-    data.set_xb = np.dot(data.inverse_matrix,data.b)
-    data.set_y = np.dot(-data.c[data.in_base],data.inverse_matrix)
-    data.set_z= np.dot(data.y,data.b)
+    data.carry.set_xb = np.dot(data.inverse_matrix,data.b)
+    data.carry.set_y = np.dot(-data.c[data.in_base],data.inverse_matrix)
+    data.carry.jset_z= np.dot(data.y,data.b)
 
+    while True :
 
+        #determina le variabili fuori base
+        compute_out_of_base(data)
+
+        #calcola i costi ridotti e trova quello negativo con indice minore
+        cost,ent_var = determine_entering_var(data)
+        if cost == None: 
+            return "trovato ottimo"      #TODO
+        
+        #verifica condizioni illimitatezza
+        Aj = np.dot(data.inverse_matrix,data.A[:,ent_var])
+        if (Aj<=0).all() :
+            return 'problema illimitato inferiormente'    #TODO
+        
+        #determino la variabile uscente
+        ext_var_index = determine_exiting_var(data,Aj)
+
+        #faccio entrare ent_var e uscire ext_var
+        data.in_base[ext_var_index] = ent_var
+
+        #cambio di base
+        h = ext_var_index
+        data.matrix = data.matrix[h+1]/Aj[h]
+        for i in range(data.matrix.shape[0]):
+            if i != h+1:
+                if i>0 :
+                    data.matrix[i] = data.matrix[i]-data.matrix[h+1]*Aj[h]
+                else:
+                    data.matrix[i] = data.matrix[i]-data.matrix[h+1]*cost
+    
+    
