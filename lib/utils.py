ff52d0e33fa9806fa43d5808f4e80d5da3455c05
Alessandro Stockman <alessandrostockman@gmail.com>
2021-05-31 15:51:10 +0200

    add plotting function

--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,9 @@
 from enum import Enum
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 
 class Parameters:
     DECIMAL_PRECISION = 13
-    
 
 class SimplexSolution(Enum):
     FINITE = 1
@@ -18,3 +19,28 @@ class DomainConstraintType(Enum):
 class DomainOptimizationType(Enum):
     MAX = 1
     MIN = 2
+
+def plot_map(image_path, blocks, columns, sols):
+    w, h = 100, 80      # Size of the box containing the values to plot
+    fontsize = 7        # Size of the font
+    padding_left = 5    # Left padding on the box
+
+    cols_num = len(columns)
+    vert_offset = 2 * h / (cols_num * 2 + 1)
+    img = plt.imread(image_path)
+    fig, ax = plt.subplots(1)
+    ax.set_aspect('equal')
+    ax.imshow(img)
+
+    for b_index, block in enumerate(blocks):
+        x, y = block['coord'].values()
+        left_x, top_y = x - w / 2, y - h / 2
+        background = Rectangle((left_x, top_y), w, h, color="black")
+        ax.add_patch(background)
+
+        offset = vert_offset
+        for c_index, column in enumerate(columns):
+            ax.text(left_x + padding_left, top_y + offset, column['name'] + ": " + str(sols[b_index * cols_num + c_index]), fontsize=fontsize, va='center', color="white")
+            offset += vert_offset
+            
+    plt.show()
