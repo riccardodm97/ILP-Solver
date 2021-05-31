23121ce9804f721201f17e08bfb1ffc2340b6d5e
Alessandro Stockman <alessandrostockman@gmail.com>
2021-05-31 16:09:07 +0200

    add main (for testing) backup

---
+++ main_old.py
@@ -0,0 +1,90 @@
+from lib.utils import DomainOptimizationType
+from lib.domain import DomainProblem
+from lib.simplex import simplex_algorithm
+
+int_probl = DomainProblem.from_json("tests/res/problem16.json")
+int_probl.solve()
+
+A1 = [
+    [1,  1,  1, -1,  0,  0],
+    [2,  0, -1,  1,  1,  0],
+    [3,  0,  1,  2,  0,  1]
+]
+
+c1 = [2, -3, 1, -4, 1, 0]
+
+b1 = [4,2,1]
+
+# ret = simplex_algorithm(c1, A1, b1)
+# print(ret)
+
+
+A2 = [
+    [1, -1, 3, 2],
+    [2, 1, -3, 3],
+    [3, 0, 0, 5]
+]
+
+c2 = [-1, 1, -2, 3]
+
+b2 = [4,6,10]
+
+# ret = simplex_algorithm(c2, A2, b2)
+# print(ret)
+
+A3 = [
+    [2, 4, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [0, 2, 0, 0, 1]
+]
+
+c3 = [-3, -5, 0, 0, 0]
+
+b3 = [25, 8, 10]
+
+# dp = DomainProblem.from_abc(A3,b3,c3,non_negatives=[0,1,2,3,4],is_integer=True)
+# dp.solve()
+
+
+A4 = [
+    [1, 1, 1, 0],
+    [10, 6, 0, 1],
+]
+
+c4 = [5, 17/4, 0, 0]
+
+b4 = [5, 45]
+
+# dp = DomainProblem.from_abc(A4,b4,c4,type=DomainOptimizationType.MAX,non_negatives=[0,1,2,3],is_integer=True)
+# dp.solve()
+
+
+A5 = [
+    [2, 4, 1, 0, 0],
+    [1, 0, 0, 1, 0],
+    [0, 2, 0, 0, 1],
+]
+
+c5 = [3, 5, 0, 0, 0]
+
+b5 = [25, 8, 10]
+
+# dp = DomainProblem.from_abc(A5,b5,c5,type=DomainOptimizationType.MAX,non_negatives=[0,1,2,3,4],is_integer=True)
+# dp.solve()
+
+
+A6 = [
+    [-1, 1, 1, 0, 0],
+    [3, 2, 0, 1, 0],
+    [2, 3, 0, 0, 1],
+]
+
+c6 = [0, 1, 0, 0, 0]
+
+b6 = [1, 12, 12]
+
+# dp = DomainProblem.from_abc(A6,b6,c6,type=DomainOptimizationType.MAX,non_negatives=[0,1,2,3,4],is_integer=True)
+# dp.solve()
+
+# DomainProblem.from_json('problem16.json').solve()
+
