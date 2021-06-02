edc8ea7720ce5bafd7205d6f04628637640da6c6
Alessandro Stockman <alessandrostockman@gmail.com>
2021-06-02 19:57:22 +0200

    fix tests

--- tests/test_bb.py
+++ tests/test_bb.py
@@ -10,11 +10,11 @@ class TestBranchAndBound(TestBase):
         #(self, optimum, standard_sol, var_chg_map):
         for p, solution in self._load_problems('integer'):
             std_problem, chg = p.get_standard_form()
-            ret_type, std_opt, std_sol = bb_algorithm(std_problem, chg, p.optimization_type)
+            ret_type, opt, sol = bb_algorithm(std_problem, chg, p.optimization_type)
 
             self.assertEqual(ret_type.value, solution['type'])
 
+            print(sol)
             if ret_type is ProblemSolution.FINITE:
-                opt, sol = p.get_problem_sol(std_opt, std_sol, chg)
                 self.assertEqual(opt, self._fract_to_dec(solution['optimum']))
                 self.assertTrue((sol == self._fract_to_dec(np.array(solution['values']))).all())
